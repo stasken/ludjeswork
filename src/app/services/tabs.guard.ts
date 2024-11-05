80bc2b4eb86833c234395abd84b01e57aab19276
Arno <arno_stas@hotmail.com>
2024-11-05 03:11:23 +0100

    tab shift route

--- src/app/services/tabs.guard.ts
+++
@@ -1,9 +0,0 @@
-// import { inject } from '@angular/core';
-// import { CanActivateFn, Router } from '@angular/router';
-
-// export const tabsGuard: CanActivateFn = (route, state) => {
-//   const router = inject(Router);
-//   const baseRoute = state.url.split('/')[1];
-//   router.navigateByUrl(`/${baseRoute}`, { replaceUrl: true });
-//   return false;
-// };
--- src/app/tabs/shift-list-tab/shift-list-routing.module.ts
+++ src/app/tabs/shift-list-tab/shift-list-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShiftListPage } from './shift-list.page';
 import { AddShiftComponent } from '../../components/shifts/add-shift/add-shift.component';
-// import { tabsGuard } from 'src/app/services/tabs.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ShiftListPage,
-    // canActivate: [tabsGuard]
   },
   {
     path: "toevoegen",
